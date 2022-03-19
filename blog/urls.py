75a37ec6f2a7ae35f4b6db3926370fffd557e4e3
codingchild <45235027+codingchild2424@users.noreply.github.com>
2022-03-19 15:15:41 +0900

    CBV

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('/<int:pk>/', views.single_post_page), #/blog/뒤에 int형태의 값이 붙는 url이면 single_post_page 함수를 적용하라
-    path('', views.index),
+    #path('', views.index),
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    posts = Post.objects.all().order_by('-pk') #이렇게 하면 pk값을 기준으로 역순으로 정렬
+# def index(request):
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#     posts = Post.objects.all().order_by('-pk') #이렇게 하면 pk값을 기준으로 역순으로 정렬
+
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+
+    post = Post.objects.get(pk=pk) #괄호안의 조건을 만족하는 값을 가져오라는 것
 
     return render(
         request,
